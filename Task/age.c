07b7ffe3cc962dfaa5244e0060c415b53e42612f
tanmay996 <110028920+tanmay996@users.noreply.github.com>
2022-08-23 16:25:24 +0530

    Add files via upload

---
+++ Task/age.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+
+	int x;
+	printf("Enter your age");
+	scanf("%d",&x);
+	 x++;
+	 
+	printf("Next year age %d",x);
+	x--;
+	x--;
+	printf("Previous year age %d",x);
+	
+	
+}

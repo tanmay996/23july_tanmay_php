38d604e3608445d5092a8b189f771776f04e3a37
tanmay996 <110028920+tanmay996@users.noreply.github.com>
2022-08-23 16:40:12 +0530

    Add files via upload

---
+++ Assignment/Module 3.1/Task1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("My name is Tanmay Bhunia \n");
+	printf("Birth date 16:11:1999 \n");
+	printf("My age is 23 \n");
+	printf("Tanmay Bhunia \n");
+	printf("Morbi \n");
+	
+}

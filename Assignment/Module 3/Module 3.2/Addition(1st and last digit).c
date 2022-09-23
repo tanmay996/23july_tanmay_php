bb05a5d6906eb56a20717e0d8041aecd9c082386
tanmay996 <110028920+tanmay996@users.noreply.github.com>
2022-09-23 18:17:06 +0530

    Add files via upload

---
+++ Assignment/Module 3/Module 3.2/Addition(1st and last digit).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+	int i,a,count,temp;
+	printf("enter a number :") ;
+	scanf("%d",&a);
+	temp=a%10;
+	
+	for(i=1;i<a;i++)
+	{
+	count=a%10;
+	a/=10;
+	}
+	printf("%d",a+temp);
+	
+	
+ }
+

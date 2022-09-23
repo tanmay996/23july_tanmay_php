82caf5a04ac1eacb1166c2ec0fd5179fac233509
tanmay996 <110028920+tanmay996@users.noreply.github.com>
2022-09-23 15:52:20 +0530

    Add files via upload

---
+++ Assignment/Module 3/Module 3.2/table.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+	int i=1,x,k;
+	printf("Enter any number ");
+	scanf("%d",&x);
+//	tanmay:
+for(i=1;i<=10;i++)
+
+//if(i<=10)	
+	{
+	
+	k=x*i;
+	printf("\n%d*%d=%d",x,i,k);
+
+	}
+
+//	if(i<=10)
+//		goto tanmay;
+}

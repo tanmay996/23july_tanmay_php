c0144ccc1991084550fd91a266f8715916da3247
tanmay996 <110028920+tanmay996@users.noreply.github.com>
2022-10-04 16:25:59 +0530

    Add files via upload

---
+++ Task/comparetwostringwithoutusingstringfunction.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	int i;
+	char str1[10],str2[10];
+	printf("Enter first string : ");
+	scanf("%s",&str1);
+	printf("Enter second string : ");
+	scanf("%s",&str2);
+	if(str1[i]!=str2[i])
+	{
+		printf("Strings are not equal \n");
+	}
+//for(i=0;i<10;i++)
+//{
+//	if(str1[i]==str2[i])
+//	{
+//		printf("Strings are equal \n");		
+//	}
+//		else
+//	{
+//		printf("Strings are not equal \n");
+//	}	
+//}
+}
